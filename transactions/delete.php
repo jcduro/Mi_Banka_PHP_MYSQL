af16f31d6193356f492bce5a0807193227455912
JUAN CARLOS <69373493+jcduro@users.noreply.github.com>
2026-03-22 15:16:58 -0500

    Add delete functionality for transfers

---
+++ transactions/delete.php
@@ -0,0 +1,10 @@
+<?php
+include __DIR__ . '/../conexion.php';
+
+if (isset($_GET['id'])) {
+    $stmt = $pdo->prepare("DELETE FROM transfers WHERE id = ?");
+    $stmt->execute([$_GET['id']]);
+}
+
+header("Location: list.php");
+exit;
